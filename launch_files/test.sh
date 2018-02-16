e7f7c261adc766273ab6fc0b4de87057ece0a893
Emmanuel Benazera <beniz@droidnik.fr>
2018-02-16 17:27:31 +0100

    test and train scripts for linux

---
+++ launch_files/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./bin/Caffe_BEGAN --train=0 --weights_gen=launch_files/BEGAN_Generator_iter_325000.caffemodel --z_dim=128 --h_dim=128 --hidden_dim=64 --image_size=128 --batch_size=16 --save_img=1 --num_gen=-1
---
+++ launch_files/train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./bin/Caffe_BEGAN --train=1 --weights_gen= --weights_discr= --z_dim=128 --h_dim=128 --hidden_dim=64 --image_size=128 --solver_gen=launch_files/solver_generator.prototxt --solver_discr=launch_files/solver_discriminator.prototxt --batch_size=32 --snapshot_generator= --snapshot_discriminator= --snapshot_k_t= --preview_generator=preview_values.csv --number_batch_loaded=125 --start_epoch=0 --end_epoch=25 --training_dataset=/media/data/datasets/celeba/img_align_celeba
